30e12493589a0267d7c6e92a535d915ab5d7a06f
Kirat Sandhu <sandhukirat482@gmail.com>
2026-01-17 15:55:36 +0530

    server is

--- Backend/server.js
+++ Backend/server.js
@@ -29,4 +29,4 @@ app.use("/api/hospital", hospitalRoutes);
 app.use("/api/patient", patientRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
